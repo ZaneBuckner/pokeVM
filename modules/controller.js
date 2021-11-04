1414526ae9066376b291a33f090e02a260976dad
zanebuckner <bucknerzane@gmail.com>
2021-11-03 22:31:09 -0500

    Set child elements to inherit 3D properties from parent to fix positioning issues.

--- modules/controller.js
+++ modules/controller.js
@@ -5,11 +5,14 @@ const pokemonCard = document.querySelector('.pokemon-card');
 
 //// ON WINDOW => USER REFRESHES PAGE or INITIAL LOAD
 window.addEventListener('load', (e) => {
+    const pokemonCard = document.querySelector('.pokemon-card');
     const cardFontSize = (pokemonCard.offsetWidth / 30).toFixed(2);
 
     pokemonCard.style.fontSize = `${cardFontSize}px`;
     vanillaTiltEvent();
 });
+
+
 
 
 //// ON WINDOW => USER ALTERS WINDOW SIZE (desktop)
@@ -19,8 +22,8 @@ window.addEventListener('resize', (e) => {
     const cardFontSize = (cardWidth / 30).toFixed(2);
 
     pokemonCard.style.fontSize = `${cardFontSize}px`;
-    vanillaTiltEvent();
-    // testFluidTypography(cardWidth, cardHeight, cardFontSize);
+    // vanillaTiltEvent();
+    testFluidTypography(cardWidth, cardHeight, cardFontSize);
 });
 
 
@@ -30,15 +33,16 @@ document.addEventListener('click', (e) => {
     const menuIcon = document.getElementById('menu-icon');
     const isSearchOpen = document.querySelector('.search-dropdown').getAttribute('aria-expanded');
     const isMenuOpen = document.querySelector('.menu-dropdown').getAttribute('aria-expanded');
-    // const isTiltOn = document.querySelector('.pokemon-card').getAttribute('aria-expanded');
+    const emailIcon = document.querySelector('.email-icon');
 
     const el = e.target;
     const elParent = e.target.parentNode;
 
-    if (el === searchIcon) { searchEvent().toggleSearch(isSearchOpen, isMenuOpen) };
-    if (el === menuIcon) { menuEvent(e).toggleMenu(isMenuOpen, isSearchOpen) };
+    if (el === searchIcon)  { searchEvent().toggleSearch(isSearchOpen, isMenuOpen) };
+    if (el === menuIcon)    { menuEvent().toggleMenu(isMenuOpen, isSearchOpen) };
+    if (el === emailIcon)   { handleEmailAnimation() }
     if (elParent.classList.contains('matched-item')) { searchEvent().handleSelection(elParent) };
-    // if (elParent.classList.contains('option')) { menuEvent().handleSelection(elParent, isTiltOn) };
+    if (elParent.classList.contains('option')) { menuEvent().handleSelection(elParent) };
 });
 
 
@@ -55,74 +59,13 @@ pokemonCard.addEventListener('click', (e) => paginationEvent().withClick(e));
 
 
 
-function vanillaTiltEvent() {
-    const tiltElement = document.querySelector('.tilt');
-    const parallaxElements = document.querySelectorAll('.parallax');
-    let isTiltEnabled = document.querySelector('.option-tilt').getAttribute('aria-expanded');
-    let isParallaxEnabled = document.querySelector('.option-parallax').getAttribute('aria-expanded');
-
-    if (window.innerWidth >= 700 && isTiltEnabled === 'false' && isParallaxEnabled === 'false') {
-        console.log('LARGE SCREEN');
-        enableTilt();
-        enableParallax();
-    };
-
-    if (window.innerWidth < 700 && isTiltEnabled === 'true' && isParallaxEnabled === 'true') {
-        console.log('SMALL SCREEN');
-        disableTilt();
-        disableParallax();
-    };
-        
-    function enableTilt() {
-        VanillaTilt.init(tiltElement, {
-            reverse: false,
-            glare: true,
-            'max-glare': 0.40
-        });
-        isTiltEnabled = 'true';
-    };
-
-    function disableTilt() {
-        tiltElement.vanillaTilt.destroy();
-        isTiltEnabled = 'false';
-    };
-
-    function enableParallax() {
-        parallaxElements.forEach(element => {
-            element.style.transformStyle = 'preserve-3d';
-            element.style.transform = 'perspective(1000px)';
-        });
-        isParallaxEnabled = 'true';
-    };
-    
-    function disableParallax() {
-        parallaxElements.forEach(element => {
-            element.style.transformStyle = '';
-            element.style.transform = '';
-        });
-        isParallaxEnabled = 'false';
-    };
-
-    
-    document.querySelector('.option-tilt').setAttribute('aria-expanded', isTiltEnabled);
-    document.querySelector('.option-parallax').setAttribute('aria-expanded', isParallaxEnabled);
-    
-    return {
-        enableTilt,
-        disableTilt,
-        enableParallax,
-        disableParallax
-    };
-}
-
-
+//// TOGGLE SEARCH BAR; FETCH, RENDER, & HANDLE SEARCH RESULTS
 function searchEvent() {
     const searchWrapper = document.querySelector('.search_wrapper');
     const searchInput = document.querySelector('.search-input');
     const searchDropdown = document.querySelector('.search-dropdown');
     const searchResults = document.querySelector('.search-results');
 
-    //// EXPANDS OR COLLAPSES THE SEARCH INPUT
     function toggleSearch(isSearchOpen, isMenuOpen) {
         isMenuOpen === 'true' ? menuEvent().toggleMenu() : null;
         isSearchOpen === 'false' ? openSearch() : closeSearch();
@@ -143,7 +86,6 @@ function searchEvent() {
 
         searchDropdown.setAttribute('aria-expanded', isSearchOpen);
     };
-
 
     //// COMPARES INPUT TO A LOCALLY STORED JSON OBJECT.
     async function fetchSearchData(input) {
@@ -194,7 +136,6 @@ function searchEvent() {
             let matchedId = child.classList.contains('matched-id');
             if (matchedId) { id = child.getAttribute('title') };
         };
-        console.clear();
         Pokemon.init(parseInt(id));
     }
 
@@ -206,10 +147,10 @@ function searchEvent() {
 };
 
 
+
 function menuEvent() {
     const menuDropdown = document.querySelector('.menu-dropdown');
     
-    //// EXPANDS OR COLLAPSES THE MENU DROPDOWN
     function toggleMenu(isMenuOpen, isSearchOpen) {
         isSearchOpen === 'true' ? searchEvent().toggleSearch() : null;
         isMenuOpen === 'false' ? openMenu() : closeMenu();
@@ -227,57 +168,126 @@ function menuEvent() {
         menuDropdown.setAttribute('aria-expanded', isMenuOpen);
     };
 
-    function handleSelection(elParent, isTiltOn) {
+    function handleSelection(elParent) {
         let option = elParent.title;
 
-        if (option === 'Filters')   { showFilters(elParent) };
-        if (option === 'Tilt')      { toggleTilt(isTiltOn) };
-        if (option === 'Favorites') { showFavorites(elParent) };
+        if (option === 'Tilt')      { toggleTilt() };
+        if (option === 'Parallax')  { toggleParallax() };
+        if (option === 'Filters')   { showFilters() };
 
-        function showFilters(elParent) {
-            console.log(('Filters Option Selected'));
-            setTimeout(function () {
-                const destination = document.querySelector('.test');
-                const pokemonCard = document.querySelector('.pokemon-card');
-                let pokemonCardCopy = pokemonCard.cloneNode(true);
-                destination.appendChild(pokemonCardCopy);
-            }, 100);
-        }
-    
-        // function toggleTilt(isTiltOn) {
-        //     console.log(isTiltOn)
-        //     let pokemonCard = document.querySelector('.pokemon-card');            
-        //     const tilt = document.querySelector('.tilt');
+        function toggleTilt() {
+            let isTiltEnabled = document.querySelector('.option-tilt').getAttribute('aria-expanded');
+            
+            if (isTiltEnabled === 'true') {
+                vanillaTiltEvent().disableTilt();
+                isTiltEnabled = 'false';
+            }
+            else {
+                vanillaTiltEvent().enableTilt();
+                isTiltEnabled = 'true';
+            };
 
-        //     if (isTiltOn) {
-        //         // tilt.vanillaTilt.destroy();
-        //         pokemonCard.classList.remove('tilt');
-        //         isTiltOn = 'false';
-        //         // return;
-        //     }
-        //     else {
-        //         // VanillaTilt.init(tilt);
-        //         // pokemonCard.classList.add('tilt');
-        //         // isTiltOn = 'true';
-        //     }
-        //     // isTiltOn = true;
+            document.querySelector('.option-tilt').setAttribute('aria-expanded', isTiltEnabled);
+        };
+        
+        function toggleParallax() {
+            let isParallaxEnabled = document.querySelector('.option-parallax').getAttribute('aria-expanded');
 
-        //     // console.log('Tilt Option Selected');
-        //     // pokemonCard.setAttribute('aria-expanded', isTiltOn);
-        //     // console.log(isTiltOn);
+            if (isParallaxEnabled === 'true') {
+                vanillaTiltEvent().disableParallax();
+                isParallaxEnabled = 'false';
+            }
+            else {
+                vanillaTiltEvent().enableParallax();
+                isParallaxEnabled = 'true';
+            }
+            
+            document.querySelector('.option-parallax').setAttribute('aria-expanded', isParallaxEnabled);
+        };
+
+        function showFilters() {
+            console.log(`DISPLAY FILTERS`)
+        };
+
+        // function showFilters(elParent) {
+        //     console.log(('Filters Option Selected'));
+        //     setTimeout(function () {
+        //         const destination = document.querySelector('.test');
+        //         const pokemonCard = document.querySelector('.pokemon-card');
+        //         let pokemonCardCopy = pokemonCard.cloneNode(true);
+        //         destination.appendChild(pokemonCardCopy);
+        //     }, 100);
         // }
     
-        function showFavorites(elParent) {
-            console.log(elParent);
-            console.log('Favorites Option Selected');
-        }
-    }
+        // function showFavorites(elParent) {
+        //     console.log(elParent);
+        //     console.log('Favorites Option Selected');
+        // }
+    };
 
     return {
         toggleMenu,
         handleSelection
-    }
+    };
 };
+
+
+function vanillaTiltEvent() {
+    const tiltElement = document.querySelector('.tilt');
+    const parallaxElements = document.querySelectorAll('.parallax');
+    let isTiltEnabled = document.querySelector('.option-tilt').getAttribute('aria-expanded');
+    let isParallaxEnabled = document.querySelector('.option-parallax').getAttribute('aria-expanded');
+
+    if (window.innerWidth >= 700 && isTiltEnabled === 'false' && isParallaxEnabled === 'false') {
+        enableTilt();
+        enableParallax();
+    };
+
+    if (window.innerWidth < 700 && isTiltEnabled === 'true' && isParallaxEnabled === 'true') {
+        disableTilt();
+        disableParallax();
+    };
+        
+    function enableTilt() {
+        VanillaTilt.init(tiltElement, {
+            reverse: false,
+            glare: true,
+            'max-glare': 0.40
+        });
+        isTiltEnabled = 'true';
+    };
+
+    function disableTilt() {
+        tiltElement.vanillaTilt.destroy();
+        isTiltEnabled = 'false';
+    };
+
+    function enableParallax() {
+        parallaxElements.forEach(element => {
+            // element.style.transform = 'perspective(1000px)';
+            // element.style.transformStyle = 'preserve-3d';
+        });
+        isParallaxEnabled = 'true';
+    };
+    
+    function disableParallax() {
+        parallaxElements.forEach(element => {
+            // element.style.transformStyle = '';
+            // element.style.transform = '';
+        });
+        isParallaxEnabled = 'false';
+    };
+    
+    document.querySelector('.option-tilt').setAttribute('aria-expanded', isTiltEnabled);
+    document.querySelector('.option-parallax').setAttribute('aria-expanded', isParallaxEnabled);
+    
+    return {
+        enableTilt,
+        disableTilt,
+        enableParallax,
+        disableParallax
+    };
+}
 
 
 function paginationEvent() {
@@ -307,7 +317,6 @@ function paginationEvent() {
 };
 
 
-
 function testFluidTypography(cardWidth, cardHeight, cardFontSize) {
     console.clear();
     const htmlFontSize = window.getComputedStyle(document.documentElement).fontSize;
@@ -325,23 +334,17 @@ function testFluidTypography(cardWidth, cardHeight, cardFontSize) {
 };
 
 
+function handleEmailAnimation() {
+    const emailText = document.querySelector('.email-text');
+    emailText.classList.add('slide-out-animation');
+};
 
+
+
+//// FOR IMPLEMENTING FAVORITE CARDS
 // setTimeout(function () {
 //     const destination = document.querySelector('.test');
 //     const pokemonCard = document.querySelector('.pokemon-card');
 //     let pokemonCardCopy = pokemonCard.cloneNode(true);
 //     destination.appendChild(pokemonCardCopy);
 // }, 100);
-
-
-
-// pokemonCard.addEventListener('click', (e) => {
-
-//     // let rippleEffect = document.createElement('span');
-//     // rippleEffect.classList.add('ripple-effect');
-//     // rippleEffect.style.left = `${x}px`;
-//     // rippleEffect.style.top = `${y}px`;
-//     // pokemonCard.appendChild(rippleEffect);
-    
-//     // setTimeout(() => rippleEffect.remove(), 500);
-// });
